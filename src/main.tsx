6d94094ff05d5ecbdf2eb8c33971be1b69ed9d43
maxgnt <155428888+maxgnt@users.noreply.github.com>
2025-04-24 10:12:21 +0200

    remove node_modules

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
 import App from './App';
 import './index.css';
+import { BrowserRouter } from 'react-router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
